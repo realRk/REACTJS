12ac4493afe77e9dafb3ee53b3657138ccceab57
rakesh-accubits <113332662+rakesh-accubits@users.noreply.github.com>
2023-09-13 22:43:43 +0530

    useRef hook implementation sample

--- hooks-demo/src/hooks/URhook.js
+++ hooks-demo/src/hooks/URhook.js
@@ -1,7 +1,21 @@
+import { useEffect, useRef, useState } from "react";
+
 const URhook = () => {
+  const [current, setCurrent] = useState("");
+  const prev = useRef("");
+  const onInputChnage = (e) => {
+    setCurrent(e.target.value);
+  };
+  useEffect(() => {
+    prev.current = current;
+  }, [current]);
   return (
     <div>
       <h1>UseRef hook</h1>
+      <h2>Sample</h2>
+      <input value={current} onChange={onInputChnage} />
+      <h1>Current value : {current}</h1>
+      <h1>Previous value : {prev.current}</h1>
     </div>
   );
 };
